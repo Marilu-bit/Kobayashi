796fe85e9ae2ae6602ffa3c207f9afbac4eb6730
Marilu-bit <armando.luiza.maria@gmail.com>
2025-08-06 17:17:03 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#Padrão para fazer funionar
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+#Chamr o views
+from views import *
+
+#Colocar o site no ar
+if __name__=="__main__":
+    app.run(debug=False)
+
+
